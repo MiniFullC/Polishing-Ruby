f573dd5f6e9da7bebb31f0be4dfc2a13a50176dd
MinifullC <1414273510@qq.com>
2018-10-31 21:47:59 +0800

    设置变量

---
+++ Chapter 1 Ruby first experience/area_volume.rb
@@ -0,0 +1,12 @@
+x = 10 #长
+y = 20 #宽
+z = 30 #高
+area = (x*y + y*z + z*x) * 2
+volume = x * y * z
+print "表面积 = ", area, "\n"
+print "体积 = ", volume, "\n"
+
+# Next
+puts "---------------------"
+print "表面积 = #{area}\n"
+puts "表面积 = #{area}"
